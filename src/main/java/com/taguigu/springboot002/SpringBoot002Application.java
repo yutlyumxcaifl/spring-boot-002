f3921e0bd3868aa148622bcfd735f5e31c120576
Andrew(余铁林) <yutl@inspur.com>
2019-07-18 23:19:27 +0800

    Initial commit

---
+++ src/main/java/com/taguigu/springboot002/SpringBoot002Application.java
@@ -0,0 +1,14 @@
+package com.taguigu.springboot002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+
+@SpringBootApplication
+public class SpringBoot002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot002Application.class, args);
+
+	}
+}
